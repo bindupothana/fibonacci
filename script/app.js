1ca1cd335503ee68d40c752409a8a112e3488230
bindupothana <himabindu.pothana@navaratan.com>
2017-03-23 17:06:22 +0530

    Fibonacci series using for

--- script/app.js
+++ script/app.js
@@ -6,15 +6,14 @@ app.controller('fibCtrl',function($scope){
 	 $scope.result = [];
    	 $scope.result.push(a)
 	 $scope.result.push(b)
-	   for ( var i=2; i<=$scope.num; i++){
-		 console.log("output",output)
-		 output=a+b;
-			a=b;
-			b=output;
-			
-         $scope.result.push(output)
+	  for ( var i=2; i<=$scope.num; i++){
+	   console.log("output",output)
+	   output=a+b;
+	   a=b;
+	   b=output;
+	   $scope.result.push(output)
 
-		}
+	  }
        console.log("Result", $scope.result)
 
 	}
